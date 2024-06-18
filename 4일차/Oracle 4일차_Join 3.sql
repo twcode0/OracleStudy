5b2a3e60a7f38e5815b46a5a8fc7eb252e334151
kimtaewoo <twcode0@gmail.com>
2024-06-18 17:46:02 +0900

    [ADD] 오라클 4일차 - Function, Set Operator, JOIN 기초
    1. 오라클 함수
     - 문자 처리 함수를 정리 한 후 최종 실습 문제 풀이
     - 그룹함수 정리 및 Group By & Having 실습
     - ROLLUP & CUBE 집계함수 정리
     2. 집합 연산자(Set Operator)
     - 합집함(UNION, UNIONALL), 사용시 필요조건 2가지 정리
     - 교집함 (INTERSECT)
     - 차집함 (MINUS)
     - 집합은 ResultSet임
    3. SQL의 꽃, JOIN
    - JOIN의 기본개념
    - JOIN을 사용하기 위한 기본 문법

---
+++ 4일차/Oracle 4일차_Join 3.sql
@@ -0,0 +1,101 @@
+-- 4일차 오라클 조인
+-- 1. JOIN
+--  - 두개 이상의  테이블에서 연관성을 가지고 있는 데이터들을
+--    따로 분류하여 새로운 가상의 테이블을 만듦
+--  → 여러 테이블의 레코드를 조합하여 하나의 레코드로 만듦
+-- ANSI 표준 구문
+-- 오라클 전용 구문
+
+-- ANSI 표준 구문
+SELECT EMP_ID, EMP_NAME, DEPT_CODE , DEPT_ID, DEPT_TITLE
+FROM EMPLOYEE
+JOIN DEPARTMENT
+ON DEPT_CODE =DEPT_ID;
+
+SELECT DEPT_ID, DEPT_TITLE FROM DEPARTMENT;
+
+SELECT EMP_ID, EMP_NAME, E.JOB_CODE, J.JOB_CODE, JOB_NAME
+FROM EMPLOYEE E
+JOIN JOB J
+USING(JOB_CODE); -- 컬럼명이 같을때 사용가능
+
+-- ORACLE 전용 구문
+SELECT EMP_ID, EMP_NAME, EMPLOYEE.JOB_CODE, JOB.JOB_CODE, JOB_NAME
+FROM EMPLOYEE 
+, JOB -- JOIN 대신 ,
+WHERE EMPLOYEE.JOB_CODE = JOB.JOB_CODE; -- ON 대신 WHERE
+
+SELECT JOB_CODE, JOB_NAME FROM JOB;
+
+-- @실습문제1
+-- 부서명과 지역명을 출력하세요.
+-- 부서명 JOB JOB_NAME, LOCATION LOCAL_NAME
+-- DEPARTMENT - DEPT_TITLE, LOCATION_ID// LOCATION - LOCAL_CODE LOCAL_NAME
+SELECT DEPT_TITLE, LOCAL_NAME
+FROM DEPARTMENT 
+JOIN LOCATION 
+ON LOCATION_ID = LOCAL_CODE;
+
+-- @실습문제2
+-- 사원명과 직급명을 출력하세요!
+-- EMPLOYEE - EMP_NAME, JOB_CODE // JOB - JOB_CODE, JOB_NAME
+SELECT EMP_NAME, JOB_NAME
+FROM EMPLOYEE E
+JOIN JOB J
+USING(JOB_CODE);
+
+-- @실습문제3
+-- 지역명과 국가명을 출력하세요
+-- LOCATION - LOCAL_NAME, NATIONAL_CODE // NATIONAL - NATIONAL_CODE, NATIONAL_NAME
+SELECT LOCAL_NAME, NATIONAL_NAME
+FROM LOCATION 
+JOIN NATIONAL 
+USING(NATIONAL_CODE);
+
+-- @JOIN 종합실습
+--1. 주민번호가 1970년대 생이면서 성별이 여자이고, 
+-- 성이 전씨인 직원들의 사원명, 주민번호, 부서명, 직급명을 조회하시오.
+-- EMPLOYEE - EMP_NAME, EMP_NO, DEPT_CODE, JOB_CODE// DEPARTMENT - DEPT_ID, DEPT_TITLE // JOB - JOB_CODE, JOB_NAME
+SELECT EMP_NAME, EMP_NO, DEPT_TITLE, JOB_NAME
+FROM EMPLOYEE
+JOIN DEPARTMENT
+ON DEPT_CODE = DEPT_ID
+JOIN JOB
+USING(JOB_CODE);
+
+--2. 이름에 '형'자가 들어가는 직원들의 사번, 사원명, 부서명을 조회하시오.
+SELECT EMP_ID, EMP_NAME, DEPT_TITLE
+FROM EMPLOYEE
+JOIN DEPARTMENT
+ON DEPT_CODE = DEPT_ID;
+
+--3. 해외영업부에 근무하는 사원명, 직급명, 부서코드, 부서명을 조회하시오.
+-- NOT KO -- JOIN NATIONAL JOIN LOCATION?
+SELECT EMP_NAME, JOB_NAME, DEPT_CODE, DEPT_TITLE
+FROM EMPLOYEE
+JOIN JOB
+USING(JOB_CODE)
+JOIN DEPARTMENT
+ON DEPT_CODE = DEPT_ID;
+
+--4. 보너스포인트를 받는 직원들의 사원명, 보너스포인트, 부서명, 근무지역명을 조회하시오.
+-- BONUS NOT NULL
+SELECT EMP_NAME, BONUS, DEPT_TITLE, LOCAL_NAME
+FROM EMPLOYEE E
+JOIN DEPARTMENT D
+ON DEPT_CODE = DEPT_ID
+JOIN LOCATION L
+ON  D.LOCATION_ID= L.LOCAL_CODE;
+
+--5. 부서코드가 D2인 직원들의 사원명, 직급명, 부서명, 근무지역명을 조회하시오.
+
+--6. 급여등급테이블의 최대급여(MAX_SAL)보다 많이 받는 직원들의 사원명, 직급명, 급여, 연봉을 조회하시오.
+-- (사원테이블과 급여등급테이블을 SAL_LEVEL컬럼기준으로 조인할 것)
+-- 데이터 없음!
+
+--7. 한국(KO)과 일본(JP)에 근무하는 직원들의 사원명, 부서명, 지역명, 국가명을 조회하시오.
+
+--8. 보너스포인트가 없는 직원들 중에서 직급이 차장과 사원인 직원들의 사원명, 직급명, 급여를 조회하시오. 
+--단, join과 IN 사용할 것
+
+--9. 재직중인 직원과 퇴사한 직원의 수를 조회하시오.
